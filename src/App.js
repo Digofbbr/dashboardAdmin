6c3ffd950370d60c086f9df8f75d191a2de251e5
Rodrigo Barreto <rodrigofb06@gmail.com>
2022-05-22 21:31:14 -0300

    navbar pronto

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import {TooltipComponent} from '@syncfusion/ej2-react-popups'
 import {Navbar, Footer, Sidebar, ThemeSettings} from './components'
 import {Ecommerce, Line,Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor} from './pages'
 
+import { useStateContext } from "./contexts/ContextProvider";
 import './App.css';
 
 
 
 const App = () => {
 
-	const activeMenu = true
+	const {activeMenu} = useStateContext()
 
   	return (
 		<div>
