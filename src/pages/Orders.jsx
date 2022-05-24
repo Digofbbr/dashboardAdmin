965e31af01dec47af674e6a5d0aab0ef59c0a322
Rodrigo Barreto <rodrigofb06@gmail.com>
2022-05-24 18:45:08 -0300

    Páginas com tabelas, kanban, editor e color picker prontos

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { GridComponent, ContextMenu, ColumnsDirective, ColumnDirective,Filter,Inject, Sort, Page, ExcelExport, PdfExport, Edit, Resize  } from '@syncfusion/ej2-react-grids';
+import {ordersData, contextMenuItems, ordersGrid} from '../data/dummy'
+import { Header} from '../components'
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Orders"  />
+
+      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging={true} allowSorting={true}>
+        <ColumnsDirective>
+          {ordersGrid.map((order, index) => (
+            <ColumnDirective key={index} {...order}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, Filter, Page, ExcelExport, Edit, PdfExport, ContextMenu]}/>
+      </GridComponent>
+    </div>
   )
 }
 
