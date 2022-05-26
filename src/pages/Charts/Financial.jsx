383e1ab645eb20fd60c834ca706c7a178682b5b9
Rodrigo Barreto <rodrigofb06@gmail.com>
2022-05-26 09:13:09 -0300

    todas as telas de gráficos feitas

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import {ChartComponent, SeriesCollectionDirective, SeriesDirective, HiloSeries ,Tooltip, Inject, DateTime, Zoom, Logarithmic, Crosshair} from '@syncfusion/ej2-react-charts'
+import {financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis} from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
+import { Header } from '../../components'
+
+
+const date1 = new Date('2017, 1, 1');
+
+function filterValue(value) {
+  if (value.x >= date1) {
+    return value.x, value.high, value.low;
+  }
+}
+const returnValue = financialChartData.filter(filterValue);
+
 
 const Financial = () => {
+
+  const {currentMode} = useStateContext()
+
   return (
-    <div>Financial</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category="Financial" title="Apple Value" />
+      <ChartComponent background={currentMode === "Dark" ? '#33373E' : '#FFF'} id='financial-chart' height='420px' primaryXAxis={FinancialPrimaryXAxis} primaryYAxis={FinancialPrimaryYAxis} chartArea={{border: {width: 0}}} tooltip={{enable: true}}>
+        <Inject services={[HiloSeries, DateTime, Tooltip, Zoom, Logarithmic, Crosshair]}/>
+        <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={returnValue}
+              xName="x"
+              yName="low"
+              name="Apple Inc"
+              type="Hilo"
+              low="low"
+              high="high"
+            />
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
   )
 }
 
